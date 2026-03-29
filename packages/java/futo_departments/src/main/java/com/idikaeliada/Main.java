4c68c1b53a3043d26a10329deb9f9966a12a6c02
IdikaEliada <idika1996@gmail.com>
2026-03-29 19:10:48 +0100

    set up java and php properly

---
+++ packages/java/futo_departments/src/main/java/com/idikaeliada/Main.java
@@ -0,0 +1,7 @@
+package packages.java.futo_departments.src.main.java.com.idikaeliada;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
--- packages/java/futo_departments/src/main/java/com/idikaibiam/utils/FutoDepartments.java
+++
@@ -1,5 +0,0 @@
-package packages.java.futo_departments.src.main.java.com.idikaibiam.utils;
-
-public class FutoDepartments {
-  
-}
