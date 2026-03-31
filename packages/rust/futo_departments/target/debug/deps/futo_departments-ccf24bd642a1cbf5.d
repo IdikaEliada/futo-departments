ecd5bbf971200b13a80520e27fb8acc867bd57fc
IdikaEliada <idika1996@gmail.com>
2026-03-31 11:58:48 +0100

    fixed rust errors

---
+++ packages/rust/futo_departments/target/debug/deps/futo_departments-ccf24bd642a1cbf5.d
@@ -0,0 +1,5 @@
+c:\Users\Ubalaochichibunkem\Desktop\uba\Idika\futo-departments\packages\rust\futo_departments\target\debug\deps\futo_departments-ccf24bd642a1cbf5.d: src\lib.rs
+
+c:\Users\Ubalaochichibunkem\Desktop\uba\Idika\futo-departments\packages\rust\futo_departments\target\debug\deps\libfuto_departments-ccf24bd642a1cbf5.rmeta: src\lib.rs
+
+src\lib.rs:
---
+++ packages/rust/futo_departments/target/debug/deps/futo_departments-dff8728fbc1182e1.d
@@ -0,0 +1,5 @@
+c:\Users\Ubalaochichibunkem\Desktop\uba\Idika\futo-departments\packages\rust\futo_departments\target\debug\deps\futo_departments-dff8728fbc1182e1.d: src\lib.rs
+
+c:\Users\Ubalaochichibunkem\Desktop\uba\Idika\futo-departments\packages\rust\futo_departments\target\debug\deps\libfuto_departments-dff8728fbc1182e1.rmeta: src\lib.rs
+
+src\lib.rs:
